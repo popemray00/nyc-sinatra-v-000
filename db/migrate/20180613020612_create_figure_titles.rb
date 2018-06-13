072873c33c92220c0fa8f150611b18a1f285f93b
Michael Pope <popemray1986@gmail.com>
2018-06-13 02:09:54 +0000

    automatically backed up by learn

--- db/migrate/20180613020612_create_figure_titles.rb
+++ db/migrate/20180613020612_create_figure_titles.rb
@@ -1,7 +1,8 @@
 class CreateFigureTitles < ActiveRecord::Migration
   def change
-    create_table :titles do |t|
-      t.string :name
+    create_table :figure_titles do |t|
+      t.integer :title_id
+      t.integer :figure_id
     end
   end
 end
